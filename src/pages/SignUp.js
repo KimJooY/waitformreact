9971a3c7df268f26a6fa3fd54ced493fafdee553
KimJooY <wndud0627@gmail.com>
2022-04-19 19:19:54 +0900

    for semi final

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -65,7 +65,7 @@ const SignUp = (props) =>{
         else
             setCanSignUp(false);
         
-        console.log("*****************************");
+        console.log("******************************");
         console.log("emailError : " + emailError);
         console.log("passWordError : "+ passWordError);
         console.log("passwordValError : " + passWordValError);
@@ -95,7 +95,7 @@ const SignUp = (props) =>{
        event.preventDefault();
        console.log(nickName+" "+password+" "+email);
         axios
-            .post("http://3.36.49.50:8080//auth/signup",{
+            .post("http://3.36.49.50:8080/auth/signup",{
                 "email" : email,
                 "nickname" : nickName,
                 "password" : password
